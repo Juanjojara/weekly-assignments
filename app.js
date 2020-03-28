57596d5abb850bdf30882ac5ff796557f548beac
Juan José JAra Laconich <juanjojara@gmail.com>
2020-03-28 23:50:08 +0100

    fixed index path

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 //app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/', (req, res) => res.sendFile("D:/Sites/idea-school/index.html"));
+app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
