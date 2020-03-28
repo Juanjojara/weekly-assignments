685367f445950512d92fea22fd1b4f0f3fbddc87
Juan José JAra Laconich <juanjojara@gmail.com>
2020-03-28 23:42:43 +0100

    fixed port

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const port = 80
+const PORT = process.env.PORT || 3000;
 
 //app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/', (req, res) => res.sendFile("D:/Sites/idea-school/index.html"));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
